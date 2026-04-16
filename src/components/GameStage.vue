be6a49e106b68c4368fa1b11c8dee7138552fc49
Ryan Grosso <ryangrosso@outlook.com>
2026-04-16 19:51:13 +0100

    Added a restart game button and cheat code

--- src/components/GameStage.vue
+++ src/components/GameStage.vue
@@ -3,6 +3,9 @@
     <GameIntro v-if="showIntro" @startGame="startGame" />
     <template v-else>
       <GameTimer ref="timerEl" />
+      <button class="restart-game" @click="restartGame">
+        <span class="mdi mdi-refresh" /><div class="sr-only">Restart game</div>
+      </button>
       <div class="peg-container">
         <div v-for="(row, r) in gameBoardTiled" :key="`row-${r}`" class="row">
           <PegTile
@@ -23,6 +26,7 @@
         :text="[
           'Congratulations!',
           'You\'ve won the game!',
+          `Your time was ${typeof gameSecondsDuration ==='number'  ? gameSecondsDuration + ' seconds' : 'N/A'}.`,
         ]"
         restartGameText="Play Again"
         @restartGame="restartGame"
@@ -34,6 +38,7 @@
         :text="[
           'There are no possible moves left to play.',
           `You had ${remainingPegs} remaining pegs left.`,
+          `Your time was ${typeof gameSecondsDuration ==='number'  ? gameSecondsDuration + ' seconds' : 'N/A'}.`,
         ]"
         restartGameText="Retry"
         @restartGame="restartGame"
@@ -58,7 +63,7 @@ const gameStatus = ref<GameStatus>(GameStatus.Ongoing);
 const remainingPegs = computed<number>(() => gameBoard.value.filter(peg => [TileState.Peg, TileState.SelectedPeg].includes(peg)).length);
 const showIntro = ref(true);
 const timerRef = useTemplateRef("timerEl");
-const gameOverDuration = shallowRef<number | null>(null);
+const gameSecondsDuration = shallowRef<number | null>(null);
 const hasStartedTimer = shallowRef(false);
 
 const gameBoardTiled = computed(() => {
@@ -79,7 +84,7 @@ const gameBoardTiled = computed(() => {
 function updateGameState() {
   gameStatus.value = PegGame.getGameState(gameBoard.value);
   if (gameStatus.value !== GameStatus.Ongoing) {
-    gameOverDuration.value = timerRef.value?.stop() ?? null;
+    gameSecondsDuration.value = timerRef.value?.stop() ?? null;
   }
 }
 
@@ -113,6 +118,26 @@ function restartGame() {
 function startGame() {
   showIntro.value = false;
 }
+
+const cheatCode = ["ArrowUp",  "ArrowDown",  "ArrowLeft", "ArrowRight"];
+let cheatCodeIndex = 0;
+window.addEventListener("keydown", (e) => {
+  if (gameStatus.value !== GameStatus.Ongoing && showIntro.value === false) {
+    return;
+  }
+  if (e.key === cheatCode[cheatCodeIndex]) {
+    cheatCodeIndex++;
+    if (cheatCodeIndex === cheatCode.length) {
+      timerRef.value?.start();
+      // fill the game board with empty tiles except for the first tile which should be a peg
+      gameBoard.value = [TileState.Peg, TileState.Peg, ...Array(gameBoard.value.length - 2).fill(TileState.Empty)] as BoardState;
+      cheatCodeIndex = 0;
+
+    }
+  } else {
+    cheatCodeIndex = 0;
+  }
+});
 </script>
 
 <style scoped>
@@ -143,5 +168,26 @@ function startGame() {
   display: flex;
   justify-content: center;
   gap: var(--tile-gap);
+}
+
+.restart-game {
+  position: absolute;
+  top: 3rem;
+  left: 4rem;
+  font-size: 1.5rem;
+  background: none;
+  border: none;
+  color: white;
+  border-radius: 64px;
+  aspect-ratio: 1;
+  height: 2.5rem;
+  width: 2.5rem;
+  cursor: pointer;
+  transition: background-color 150ms ease-in-out;
+  border: solid 2px var(--clr-primary);
+}
+
+.restart-game:hover {
+  background-color: var(--clr-primary);
 }
 </style>
